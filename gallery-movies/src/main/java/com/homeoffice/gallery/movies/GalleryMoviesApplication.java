d30edf857beba0e50619526baa33caee63830faa
Byron Sebastian Rosero Diaz <sebasdiaz67@gmail.com>
2020-05-26 01:27:46 -0500

    Second commit

---
+++ gallery-movies/src/main/java/com/homeoffice/gallery/movies/GalleryMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.homeoffice.gallery.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalleryMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalleryMoviesApplication.class, args);
+	}
+
+}
---
+++ gallery-movies/src/test/java/com/homeoffice/gallery/movies/GalleryMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homeoffice.gallery.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalleryMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
